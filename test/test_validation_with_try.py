2a41d3f1c6ba3b80d06b315d0b05d519c80cdb23
Levi Shepherd <levi.d.shepherd@outlook.com>
2020-09-21 21:55:11 -0500

    added to test method for 2nd score negative input

--- test/test_validation_with_try.py
+++ test/test_validation_with_try.py
@@ -4,10 +4,12 @@ from input_validation.validation_with_try import *
 
 class MyTestCase(unittest.TestCase):
 
-    # Test for score1 as a negative
+    # Test for negative input
     def test_average_negative_input(self):
         with self.assertRaises(ValueError):
             average(-90, 89, 78)
+        with self.assertRaises(ValueError):
+            average(90, -89, 78)
 
 
 if __name__ == '__main__':
