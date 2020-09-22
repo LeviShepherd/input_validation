bcda17fbba7cb148aa33afd46c607999fc80aa8b
Levi Shepherd <levi.d.shepherd@outlook.com>
2020-09-21 21:50:10 -0500

    added test for negative input for score1

--- test/test_validation_with_try.py
+++ test/test_validation_with_try.py
@@ -3,8 +3,10 @@ from input_validation import validation_with_try
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def test_average_negative_input(self):
+        with self.assertRaises(ValueError):
+            average(-90, 89, 78)
 
 
 if __name__ == '__main__':
